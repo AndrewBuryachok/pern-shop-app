83f9172a6f4f3d78d0da1dc757335b951e9afe9f
AndrewBuryachok <andrewburyachok02@gmail.com>
2023-11-21 12:40:44 -0500

    Client: Fix models in cities

--- client/src/features/cities/city.model.ts
+++ client/src/features/cities/city.model.ts
@@ -1,10 +1,12 @@
 import { Place, PlaceWithUser, SmPlaceWithUser } from '../places/place.model';
 import { SmUser } from '../users/user.model';
 
-export interface SmCity extends PlaceWithUser {}
+export interface SmCity extends Place {}
 
 export interface SmCityWithUser extends SmPlaceWithUser {}
 
-export interface City extends PlaceWithUser {
+export interface MdCity extends PlaceWithUser {}
+
+export interface City extends MdCity {
   users: SmUser[];
 }
--- client/src/features/users/user.model.ts
+++ client/src/features/users/user.model.ts
@@ -1,4 +1,4 @@
-import { SmCity } from '../cities/city.model';
+import { MdCity } from '../cities/city.model';
 import { SmCard } from '../cards/card.model';
 
 export interface SmUser {
@@ -16,7 +16,7 @@ export interface MdUser extends SmUser {
 
 export interface User extends MdUser {
   registeredAt: Date;
-  city?: SmCity;
+  city?: MdCity;
   cards: SmCard[];
 }
 
