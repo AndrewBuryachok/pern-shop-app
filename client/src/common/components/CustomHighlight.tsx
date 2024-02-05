27d6ae3bf5a833b21459499ed23e26854214c044
AndrewBuryachok <andrewburyachok02@gmail.com>
2024-02-05 05:00:31 -0500

    Client: Add markdown in text

--- client/src/common/components/CustomHighlight.tsx
+++ client/src/common/components/CustomHighlight.tsx
@@ -1,18 +1,32 @@
-import { Highlight } from '@mantine/core';
+import showdown from 'showdown';
+import { TypographyStylesProvider } from '@mantine/core';
 
 type Props = {
   text: string;
 };
 
 export default function CustomHighlight(props: Props) {
+  const converter = new showdown.Converter({
+    openLinksInNewWindow: true,
+    simplifiedAutoLink: true,
+    simpleLineBreaks: true,
+    strikethrough: true,
+    underline: true,
+    tasklists: true,
+    tables: true,
+    emoji: true,
+    ghMentions: true,
+    ghMentionsLink: '/users/{u}',
+  });
+
   return (
-    <Highlight
-      size='xs'
-      highlightColor='violet'
-      highlight={/@\w+/.exec(props.text) || []}
-      style={{ whiteSpace: 'pre-wrap' }}
-    >
-      {props.text}
-    </Highlight>
+    <TypographyStylesProvider>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: converter.makeHtml(props.text),
+        }}
+        style={{ fontSize: 14, marginBottom: -20 }}
+      />
+    </TypographyStylesProvider>
   );
 }
