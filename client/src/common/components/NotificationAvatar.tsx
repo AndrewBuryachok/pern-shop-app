b063b4ecedb631ba6af905fcd7744cac7642ebde
AndrewBuryachok <andrewburyachok02@gmail.com>
2025-02-28 16:04:21 +0200

    Client: Refactor notifications from system

--- client/src/common/components/NotificationAvatar.tsx
+++ client/src/common/components/NotificationAvatar.tsx
@@ -1,10 +1,19 @@
-import { Skeleton } from '@mantine/core';
+import { Skeleton, ThemeIcon } from '@mantine/core';
+import { IconBell } from '@tabler/icons';
 import { useSelectAllUsersQuery } from '../../features/users/users.api';
 import LinkedAvatar from './LinkedAvatar';
 
 type Props = { nick: string };
 
 export default function NotificationAvatar(props: Props) {
+  if (props.nick === '🔔') {
+    return (
+      <ThemeIcon size={32}>
+        <IconBell size={24} />
+      </ThemeIcon>
+    );
+  }
+
   const { data: users } = useSelectAllUsersQuery();
 
   const user = users?.find((user) => user.nick === props.nick);
