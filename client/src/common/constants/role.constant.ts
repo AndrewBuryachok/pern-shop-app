1ec28ee2bd2404478630ab1a1f7b6bb06808314b
AndrewBuryachok <andrewburyachok02@gmail.com>
2025-11-20 15:12:13 +0200

    Client: Remove spawn, hub, end from roles

--- client/src/common/constants/role.constant.ts
+++ client/src/common/constants/role.constant.ts
@@ -1,10 +1,7 @@
 export enum Role {
   ADMIN = 'admin',
   MODER = 'moder',
-  SPAWN = 'spawn',
-  HUB = 'hub',
-  END = 'end',
   STREAMER = 'streamer',
 }
 
-export const rolesToColors = ['red', 'blue', 'teal', 'pink', 'grape', 'violet'];
+export const rolesToColors = ['red', 'blue', 'violet'];
