5738f1b577e422a30cd16ea1632287c44286915a
AndrewBuryachok <andrewburyachok02@gmail.com>
2025-11-23 08:22:19 +0200

    Client: Add browser image compression

--- client/src/common/utils/upload.util.ts
+++ client/src/common/utils/upload.util.ts
@@ -1,9 +1,17 @@
 import axios from 'axios';
+import imageCompression from 'browser-image-compression';
 
 export const uploadImage = async (file: File) => {
   try {
+    const compressed = await imageCompression(file, {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+      fileType: 'image/webp',
+      initialQuality: 0.8,
+    });
     const formData = new FormData();
-    formData.append('image', file);
+    formData.append('image', compressed, `${crypto.randomUUID()}.webp`);
     const response = await axios.post(
       `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_KEY}`,
       formData,
