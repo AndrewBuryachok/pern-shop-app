4fbf3075919f7fcc763df3d4e785d1753c8eaf87
AndrewBuryachok <andrewburyachok02@gmail.com>
2026-01-12 10:14:27 +0200

    Server: Rename complete with pay in invoices

--- server/src/features/invoices/invoice-error.enum.ts
+++ server/src/features/invoices/invoice-error.enum.ts
@@ -1,7 +1,7 @@
 export enum InvoiceError {
-  ALREADY_COMPLETED = '400: Інвойс вже завершено',
+  ALREADY_COMPLETED = '400: Інвойс вже сплачено',
   NOT_RECEIVER = '403: Ви не отримувач інвойсу',
   CREATE_FAILED = '500: Не вдалося створити інвойс',
-  COMPLETE_FAILED = '500: Не вдалося завершити інвойс',
+  COMPLETE_FAILED = '500: Не вдалося сплатити інвойс',
   DELETE_FAILED = '500: Не вдалося видалити інвойс',
 }
